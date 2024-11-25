c8639d9c677e09845b1a19e85671f1b86b5e553b
Raffi Molero <raffimolero@gmail.com>
2024-11-26 02:52:47 +0800

    todo

--- php/views/index.php
+++ php/views/index.php
@@ -3,6 +3,13 @@
 <?= view('products_dyn', []) ?>
 <?php $body = ob_get_clean(); ?>
 
+<!--
+TODO:
+- category sorting
+- category deduplication
+- login cookies
+-->
+
 <?= view('template', [
     'title' => 'Home',
     'body' => $body,
