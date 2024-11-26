b0ce1854a06449604834b8fa42ba39a6694f9180
Raffi Molero <raffimolero@gmail.com>
2024-11-26 15:10:04 +0800

    remove comment

--- php/views/index.php
+++ php/views/index.php
@@ -2,13 +2,6 @@
 <?php ob_start(); ?>
 <?= view('products_dyn', []) ?>
 <?php $body = ob_get_clean(); ?>
-
-<!--
-TODO:
-- category sorting
-- category deduplication
--->
-
 <?= view('template', [
     'title' => 'Home',
     'body' => $body,
