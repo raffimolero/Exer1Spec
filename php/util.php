a5f7fc01b3e669db2149f8f6be8ad6a56cfbd6a3
Raffi Molero <raffimolero@gmail.com>
2024-11-29 01:18:02 +0800

    refactor dbg again

--- php/util.php
+++ php/util.php
@@ -1,24 +1,34 @@
 <?php
 
-function dbg($x, ...$xs)
+function pipe(mixed $x, callable ...$fs)
 {
-    var_dump([
-        'DEBUG' => [...$xs],
-        'TRACE' => debug_backtrace(),
-        'VALUE' => $x,
-    ]);
+    foreach ($fs as $f) $x = $f($x);
+    return $x;
+}
+
+function ob(callable $f)
+{
+    ob_start();
+    $f();
+    return ob_get_clean();
+}
+
+function dbg(mixed $x, mixed ...$xs)
+{
+    error_log('DEBUG:');
+    error_log(...$xs);
+    error_log(ob(fn() => debug_print_backtrace()));
+
+    error_log('VALUE:');
+    error_log(print_r($x, true));
     error_log('');
     return $x;
 }
 
 // https://stackoverflow.com/a/4517270
-function extract_substr($str, $prefix, $suffix)
+function extract_substr(string $str, string $prefix, string $suffix)
 {
     $start = '^' . preg_quote($prefix, '/');
     $end = preg_quote($suffix, '/') . '$';
     return preg_replace("/$start|$end/", '', $str);
 }
-
-// chatgpt
-// Function to pretty print an associative array recursively
-function prettyPrintArray($array, $indent = 1) {}
